1bec17f1a069b470e97546ba09009358136c0862
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-08-19 16:52:56 +0600

    collection view & collection view cell design complete

---
+++ CollectionView_iOS/MovieCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieCollectionViewCell.swift
+//  CollectionView_iOS
+//
+//  Created by Sadia on 19/8/23.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    
+}
